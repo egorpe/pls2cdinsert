83955875e8c2b7bd222ce5a080ce5e2ffea6eb95
Egor Pervuninski <egorp@eneset.com>
2016-09-29 16:57:24 +1000

    Added code to search for playlist

--- src/main/groovy/re/system/pls2cdinsert/Main.groovy
+++ src/main/groovy/re/system/pls2cdinsert/Main.groovy
@@ -1,13 +1,32 @@
 package re.system.pls2cdinsert
 
+import com.dd.plist.NSDictionary
+import com.dd.plist.NSObject
+import com.dd.plist.PropertyListParser
 import org.slf4j.LoggerFactory;
 import org.slf4j.Logger;
 
 class Main {
     static Logger log = LoggerFactory.getLogger(Main.class)
+    static String ITUNES_LIBRARY_PATH = "${System.properties['user.home']}/Music/iTunes/iTunes Library.xml"
 
     public static void main(String[] args) {
         log.info('iTunes Playlist To CD Insert, Version 1.0')
+        log.info("Using iTunes library at: ${ITUNES_LIBRARY_PATH}")
+        log.info("Processing playlist #${args[0]}")
+
+        File file = new File(ITUNES_LIBRARY_PATH)
+        NSDictionary library = PropertyListParser.parse(file);
+
+        for (NSObject playlist : library.objectForKey('Playlists').array) {
+            if (playlist.'Playlist ID'.intValue() == Integer.parseInt(args[0])) {
+                processPlaylist(playlist)
+            }
+        }
+    }
+
+    static void processPlaylist(playlist) {
+        log.info("Found playlist \"${playlist.Name}\"")
     }
 }
 
